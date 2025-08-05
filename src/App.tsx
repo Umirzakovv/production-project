122c19061ce1ea841ef919ed9e5a62cccd16b96f
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2025-08-05 12:39:22 +0500

    Implement classNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import { Counter } from "./components/Counter";
-import "./index.scss";
+import { Route, Routes } from "react-router-dom";
+import "./styles/index.scss";
+import { Link } from "react-router-dom";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
-      dfghjkl
-      <Counter />
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to="/">Главная</Link>
+      <Link to="/about">О нас</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
